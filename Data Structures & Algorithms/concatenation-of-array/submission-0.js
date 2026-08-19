08a8c0dac232a0e4dde5b1c0f782580e25e3de36
Kriz-J <119603220+Kriz-J@users.noreply.github.com>
2026-08-19 14:38:58 +0200

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+
+    // getConcatenation(nums) {
+    //     let ans = nums.slice(0);
+    //     for (const num of nums){
+    //         ans.push(num);
+    //     }
+    //     return ans;
+    // }
+
+    // getConcatenation(nums){
+    //     return [...nums, ...nums];
+    // }
+
+    // getConcatenation(nums){
+    //     return nums.concat(nums);
+    // }
+
+    getConcatenation = nums => nums.concat(nums);
+}
