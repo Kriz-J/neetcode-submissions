93c4a4b146d47ab3e483073cf088dda7a7b8d509
Kriz-J <119603220+Kriz-J@users.noreply.github.com>
2026-08-19 15:27:55 +0200

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+
+    // hasDuplicate(nums) {
+    //     const map = new Map();
+    //     for (const num of nums){
+    //         if (map.has(num)){
+    //             return true;
+    //         }
+    //         map.set(num, 1);
+    //     }
+    //     return false;
+    // }
+
+    hasDuplicate(nums) {
+        const set = new Set(nums);
+        return set.size < nums.length;
+    }
+}
