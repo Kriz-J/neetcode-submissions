29bff2c25b3eeb1841d54a2a509185acd19dd1fd
Kriz-J <119603220+Kriz-J@users.noreply.github.com>
2026-08-24 13:01:42 +0200

    Add: remove-element - submission-4

---
+++ Data Structures & Algorithms/remove-element/submission-4.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let k = 0;
+        let n = nums.length - 1;
+
+        while (k <= n) {
+            if (nums[k] === val){
+                nums[k] = nums[n];
+                n--;
+            }
+            else {
+                
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
