dadc5385a9ec5274f8709a05d9e82c15020730d6
Kriz-J <119603220+Kriz-J@users.noreply.github.com>
2026-08-28 17:33:46 +0200

    Add: valid-palindrome-ii - submission-4

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-4.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    validPalindrome(s) {
+        let l = 0;
+        let r = s.length - 1;
+
+        while (l < r) {
+            if (s[l] !== s[r]) {
+                return (this.isPalindrome(s.slice(l + 1, r + 1)) || this.isPalindrome(s.slice(l, r)));
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    isPalindrome(s) {
+        let l = 0;
+        let r = s.length - 1;
+
+        while (l < r) {
+            if (s[l] !== s[r]) {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
